6436623dfac36c70a886413bc94d14abdbf1c243
repro <f2b@gmx.ch>
2013-03-03 17:47:11 +0100

    setup script updated

--- warp4_setup.sh
+++ warp4_setup.sh
@@ -3,6 +3,9 @@ xfconf-query -c xfce4-desktop -p /backdrop -r -R
 xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/backdrop-cycle-enable -s "false" -t bool -n
 xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/backdrop-cycle-timer -s "10" -t int -n
 xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/brightness -s "1" -t int -n
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/color-style -t int -s "0" -n
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/color1 -t uint16 -s 11051 -t uint16 -s 0 -t uint16 -s 43690 -t uint16 -s 65535 -n
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/color2 -t uint16 -s 11051 -t uint16 -s 0 -t uint16 -s 43690 -t uint16 -s 65535 -n
 xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-path -s "${HOME}/.local/share/xfce4/backdrops/BIGBLU.png" -t string -n
 xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-show -s "true" -t bool -n
 xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-style -s 0 -t int -n
@@ -31,7 +34,7 @@ xfconf-query -c xfce4-panel -p /panels -s 1 -t int -a -n
 xfconf-query -c xfce4-panel -p /panels/panel-1/autohide -s "false" -t bool -n
 xfconf-query -c xfce4-panel -p /panels/panel-1/background-style -s "0" -t int -n
 xfconf-query -c xfce4-panel -p /panels/panel-1/background-color -r
-#xfconf-query -c xfce4-panel -p /panels/panel-1/background-color -t int -s "52428" -t int -s "52428" -t int -s "52428" -t int -s "65535" -n
+xfconf-query -c xfce4-panel -p /panels/panel-1/background-color -t uint16 -s "52428" -t uint16 -s "52428" -t uint16 -s "52428" -t uint16 -s "65535" -n
 xfconf-query -c xfce4-panel -p /panels/panel-1/background-image -r
 xfconf-query -c xfce4-panel -p /panels/panel-1/disable-struts -s "false" -t bool -n
 xfconf-query -c xfce4-panel -p /panels/panel-1/length -s "100" -t int -n
@@ -51,6 +54,17 @@ xfconf-query -c xfce4-panel -p /plugins/plugin-1/custom-menu -s "false" -t bool 
 xfconf-query -c xfce4-panel -p /plugins/plugin-1/custom-menu-file -r -R
 xfconf-query -c xfce4-panel -p /plugins/plugin-1/button-icon -s "xfce4-panel-menu" -t string -n
 xfconf-query -c xfce4-panel -p /plugins/plugin-2 -t string -s "tasklist" -n
+xfconf-query -c xfce4-panel -p /plugins/plugin-2/show-labels -t bool -s "true" -n
+xfconf-query -c xfce4-panel -p /plugins/plugin-2/flat-buttons -t bool -s "false" -n
+xfconf-query -c xfce4-panel -p /plugins/plugin-2/show-handle -t bool -s "true" -n
+xfconf-query -c xfce4-panel -p /plugins/plugin-2/sort-order -t int -s "1" -n
+xfconf-query -c xfce4-panel -p /plugins/plugin-2/grouping -t int -s "0" -n
+xfconf-query -c xfce4-panel -p /plugins/plugin-2/switch-workspace-on-unminimize -t bool -s "false" -n
+xfconf-query -c xfce4-panel -p /plugins/plugin-2/show-wireframes -t bool -s "false" -n
+xfconf-query -c xfce4-panel -p /plugins/plugin-2/window-scrolling -t bool -s "true" -n
+xfconf-query -c xfce4-panel -p /plugins/plugin-2/include-all-workspaces -t bool -s "false" -n
+xfconf-query -c xfce4-panel -p /plugins/plugin-2/include-all-monitors -t bool -s "true" -n
+xfconf-query -c xfce4-panel -p /plugins/plugin-2/show-only-minimized -t bool -s "false" -n
 xfconf-query -c xfce4-panel -p /plugins/plugin-3 -t string -s "separator" -n
 xfconf-query -c xfce4-panel -p /plugins/plugin-3/expand -t bool -s "true" -n
 xfconf-query -c xfce4-panel -p /plugins/plugin-3/style -t int -s "0" -n
@@ -62,11 +76,33 @@ xfconf-query -c xfce4-panel -p /plugins/plugin-4/show-frame -t bool -s "false" -
 xfconf-query -c xfce4-session -p /general/SaveOnExit -t bool -s "false" -n
 xfconf-query -c xfce4-session -p /startup/ssh-agent/enabled -t bool -s false -n
 xfconf-query -c xfwm4 -p /general/button_layout -s "O|CHM" -t string -n
+xfconf-query -c xfwm4 -p /general/box_resize -s "false" -t bool -n
+xfconf-query -c xfwm4 -p /general/box_move -s "false" -t bool -n
+xfconf-query -c xfwm4 -p /general/double_click_action -s "maximize" -t string -n
 xfconf-query -c xfwm4 -p /general/snap_to_border -s "false" -t bool -n
 xfconf-query -c xfwm4 -p /general/snap_to_windows -s "false" -t bool -n
+xfconf-query -c xfwm4 -p /general/snap_width -s "10" -t int -n
+xfconf-query -c xfwm4 -p /general/wrap_workspaces -s "false" -t bool -n
+xfconf-query -c xfwm4 -p /general/wrap_windows -s "false" -t bool -n
+xfconf-query -c xfwm4 -p /general/wrap_resistance -s "10" -t int -n
 xfconf-query -c xfwm4 -p /general/theme -s "Warp4" -t string -n
 xfconf-query -c xfwm4 -p /general/title_alignment -s "left" -t string -n
 xfconf-query -c xfwm4 -p /general/title_font -s "Workplace Sans Bold 11" -t string -n
+xfconf-query -c xfwm4 -p /general/click_to_focus -s "true" -t bool -n
+xfconf-query -c xfwm4 -p /general/focus_delay -s "5" -t int -n
+xfconf-query -c xfwm4 -p /general/focus_new -s "true" -t bool -n
+xfconf-query -c xfwm4 -p /general/raise_on_focus -s "false" -t bool -n
+xfconf-query -c xfwm4 -p /general/raise_delay -s "246" -t int -n
+xfconf-query -c xfwm4 -p /general/raise_on_click -s "true" -t bool -n
+xfconf-query -c xfwm4 -p /general/cycle_minimum -s "true" -t bool -n
+xfconf-query -c xfwm4 -p /general/cycle_hidden -s "true" -t bool -n
+xfconf-query -c xfwm4 -p /general/cycle_workspaces -s "false" -t bool -n
+xfconf-query -c xfwm4 -p /general/cycle_draw_frame -s "true" -t bool -n
+xfconf-query -c xfwm4 -p /general/prevent_focus_stealing -s "false" -t bool -n
+xfconf-query -c xfwm4 -p /general/focus_hint -s "true" -t bool -n
+xfconf-query -c xfwm4 -p /general/activate_action -s "bring" -t string -n
+xfconf-query -c xfwm4 -p /general/tile_on_move -s "false" -t bool -n
+xfconf-query -c xfwm4 -p /general/use_compositing -s "false" -t bool -n
 xfconf-query -c xsettings -p /Gtk/ButtonImages -s "false" -t bool -n
 xfconf-query -c xsettings -p /Gtk/CanChangeAccels -s "false" -t bool -n
 xfconf-query -c xsettings -p /Gtk/CursorThemeName -s "Warp4" -t string -n
